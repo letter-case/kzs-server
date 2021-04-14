1c920264134d580cb3450332f1ecd3bbd6cc6280
Insaf Almukhametov <insaf_13@mail.ru>
2021-04-14 22:08:25 +0300

    replce: relegram group

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -2,11 +2,11 @@ const http = require("request");
 
 const { TELEGRAM_TOKEN } = require("../configs/envs");
 
-const chat = "-520456096";
+const chat = "-406973470";
 
 const telegram = (msg) => {
     const message = encodeURI(msg);
-    const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${chat}&parse_mode=html&text=${message}`;
+    const url = `https://api.telegram.org/bot1511672828:AAF5lqARqNp7REtsnopoUeOnsbIG5Ee-Esg/sendMessage?chat_id=${chat}&parse_mode=html&text=${message}`;
 
     http.post(url, (error, response, body) => {
         console.error("error:", error);
